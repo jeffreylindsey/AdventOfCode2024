bad7b384d81fe4497be37c669df2e82b63a40881
jeffreylindsey <9539309+jeffreylindsey@users.noreply.github.com>
2024-12-29 15:38:22 -0800

    Day 10 prep.

---
+++ Day10/Day10.cpp
@@ -0,0 +1,46 @@
+#include "Precompiled.hpp"
+
+#include "AOCTest.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Day10
+{
+
+
+constexpr std::string_view DayString = "Day10";
+
+/*****************************************************************************/
+
+TEST_CLASS(Part1)
+{
+	/*=======================================================================*/
+	int Run(std::ifstream Input)
+	{
+		return 0;
+	}
+
+	public:
+		AOC_TEST(Sample, 0)
+		AOC_TEST(Input, 0)
+};
+
+/*****************************************************************************/
+
+TEST_CLASS(Part2)
+{
+	/*=======================================================================*/
+	int Run(std::ifstream Input)
+	{
+		return 0;
+	}
+
+	public:
+		AOC_TEST(Sample, 0)
+		AOC_TEST(Input, 0)
+};
+
+/*****************************************************************************/
+
+
+}
